c0f917ccd38d3719760e80f7a31bf3eb47db7004
jhashi19 <j.hashi19@gmail.com>
2021-01-19 20:44:07 +0900

    implement reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 
-// actionを返す関数(action creater)
+// actionを返す関数(action creator)
 export const increment = () => ({
     type: INCREMENT
 });
